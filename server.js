9c6b68673dc503cb75ffd956262cb8e5e4146967
Patrick Araujo <rayssacoosta@gmail.com>
2021-01-18 23:21:32 -0300

    make it better

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
-const express = require('express');
+/*const express = require('express');
 const app = express();
 
 let port = process.env.PORT;
 if (port == null || port == "") {
   port = 8000;
 }
+app.get('/test',(req,res)=>{
+    res.end('oi')
+})
 app.listen(port);
+*/
+
+const express = require('express')
+const PORT = process.env.PORT || 5000
+
+express()
+  .get('/', (req, res) => res.end('oi'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
